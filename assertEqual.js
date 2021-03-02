dd6ea08bb5812f4d92ed9eec7723da0cd1c41dee
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-01 17:22:47 -0800

    assert equal

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
