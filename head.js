a4fe6f59f7846de559c1a0fccce4ef9a29b2fa92
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-01 17:53:02 -0800

    pushing head.js

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr) {
+  return arr[0];
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
