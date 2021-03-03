0c07037a3ad626296a390ce3a79b15a45a7ffcd6
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-02 16:48:58 -0800

    implemented assertArrayEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "4"]);
