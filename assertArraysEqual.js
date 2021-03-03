4769db03d13355dc2c9636022d68314eb0c590a7
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-03 13:00:07 -0800

    code refactor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,8 @@
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`Assertion Passed: ${array1} === ${array2}`);
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
   } else {
-    console.log(`Assertion Failed: ${array1} !== ${array2}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
