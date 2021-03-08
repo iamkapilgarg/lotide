ee99d216fad1d7f824efe0d45f85e6b61814563a
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-07 19:44:14 -0800

    added recursion logic in eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -8,14 +8,12 @@ const assertEqual = function (actual, expected) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {
-  let object1Keys = Object.keys(object1);
-  let object2Keys = Object.keys(object2);
-  if (object1Keys.length !== object2Keys.length) {
-    return false;
-  }
-  for (let key of object1Keys) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) return false;
+  for (let key in object1) {
+    if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+      return eqObjects(object1[key], object2[key]);
+    } else if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
@@ -26,7 +24,7 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   } else {
@@ -39,16 +37,17 @@ const eqArrays = function (arr1, arr2) {
   }
 };
 
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba), true); // => true
-
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false); // => false
-
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 assertEqual(eqObjects(cd, dc), true); // => true
-
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
