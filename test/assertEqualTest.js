38f249b3d1d7dbeb40048cee5c9c20db1f6d88a7
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-08 15:20:45 -0800

    added mocha and chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,3 +1,5 @@
+ const assert = require('chai').assert;
+
 const assertEqual = require('../assertEqual');
 const head = require('../head');
 const tail = require('../tail');
@@ -5,13 +7,18 @@ const eqArrays = require('../eqArray');
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
+
 //Assert Equal
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 1);
-
-//Head
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 //Tail
 const result = tail(["Hello", "Lighthouse", "Labs"]);
