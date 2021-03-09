17c3ef943d2c7c6b58b4aa3f17e3094f3669cad2
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-08 17:20:34 -0800

    more refactor

--- eqObjects.js
+++ eqObjects.js
@@ -1,5 +1,4 @@
 const eqArrays = require('./eqArray');
-const assertEqual = require('./assertEqual');
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
